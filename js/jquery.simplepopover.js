0fcc9c68373dd0acdcbc70520b1dd7e0353b2b29
Salehen Rahman <salehen.rahman@gmail.com>
2012-07-31 16:54:16 -0700

    Fixed a boat load of bugs. Also moved to version 1.1.0.

--- js/jquery.simplepopover.js
+++ js/jquery.simplepopover.js
@@ -7,64 +7,72 @@
      * @returns the DOM element being acted upon, as a jQuery object.
      */
     $.fn.simplePopover = function (options) {
-        var template = '<div class="simple-popover"><div class="content"></div><div class="arrow"></div></div>',
-            $popover = $(template),
-            self = this;
+        $.each(this, function (index, value) {
+            var $this = $(value),
+                template = '<div class="simple-popover"><div class="content"></div><div class="arrow"></div></div>',
+                $popover = $(template),
+                self = $this;
 
-        this.hover(function () {
-            var left, top;
+            $this.hover(function () {
+                var left, top;
 
-            $popover.find('.content').html(options.content);
-            $('body').append($popover);
-
-            if (options.padding != null) {
-                $popover.find('.content').css({ 'padding': options.padding + 'px' });
-            }
-
-            if (options.position === 'right') {
-                left = self.width() + self.offset().left + 10;
-                top  = Math.floor((self.outerHeight(true) - $popover.outerHeight(true)) / 2) + self.offset().top;
-
-                $popover.css({
-                    top: top + 'px',
-                    left: left + 'px'
-                });
-
-                $popover.find('.arrow').css({
-                    'margin-left': '0',
-                    'margin-top': '-5px',
-                    'border-top': '5px solid transparent',
-                    'border-bottom': '5px solid transparent',
-                    'border-right': '5px solid black',
-                    'left': '-10px',
-                    'top': '50%'
-                });
-
-            } else {
-                left = (Math.floor((self.width() - $popover.width()) / 2) + self.offset().left);
-
-                if (left < 0) {
-                    left = 0;
-                    $popover.find('.arrow').css({ 'left': '20px' });
+                if (options.content == null) {
+                    $popover.find('.content').html(self.data('content'));
+                } else {
+                    $popover.find('.content').html(options.content);
                 }
 
-                if (left + $popover.outerWidth(true) > $(window).width()) {
-                    left = $(window).width() - $popover.outerWidth(true);
-                    $popover.find('.arrow').css({ 'left': $popover.outerWidth(true) - 20 + 'px' });
+                $('body').append($popover);
+
+                if (options.padding != null) {
+                    $popover.find('.content').css({ 'padding': options.padding + 'px' });
                 }
 
-                $popover.css({
-                    top: (self.offset().top - $popover.outerHeight(true) - 10) + 'px',
-                    left: left + 'px'
+                if (options.position === 'right') {
+                    left = self.width() + self.offset().left + 10;
+                    top  = Math.floor((self.outerHeight(true) - $popover.outerHeight(true)) / 2) + self.offset().top;
+
+                    $popover.css({
+                        top: top + 'px',
+                        left: left + 'px'
+                    });
+
+                    $popover.find('.arrow').css({
+                        'margin-left': '0',
+                        'margin-top': '-5px',
+                        'border-top': '5px solid transparent',
+                        'border-bottom': '5px solid transparent',
+                        'border-right': '5px solid black',
+                        'left': '-10px',
+                        'top': '50%'
+                    });
+
+                } else {
+                    left = (Math.floor((self.width() - $popover.width()) / 2) + self.offset().left);
+
+                    if (left < 0) {
+                        left = self.offset().left;
+                        $popover.find('.arrow').css({ 'left': '20px' });
+                    }
+
+                    if (left + $popover.outerWidth(true) > $(window).width()) {
+                        left = self.offset().left - $popover.outerWidth(true) + self.width();
+                        $popover.find('.arrow').css({ 'left': $popover.outerWidth(true) - 20 + 'px' });
+                    }
+
+                    $popover.css({
+                        top: (self.offset().top - $popover.outerHeight(true) - 10) + 'px',
+                        left: left + 'px'
+                    });
+                }
+
+                $popover.fadeIn(100);
+
+            }, function () {
+                $popover.fadeOut(100, function () {
+                    $popover.remove();
+                    $popover = $(template);
                 });
-            }
-
-            $popover.fadeIn(100);
-
-        }, function () {
-            $popover.fadeOut(100, function () {
-                $popover.remove();
-                $popover = $(template);
             });
         });
 
